7a305bc84d508ff269e6b4b8ab980e02ce4c7387
pooveshn6566-hub <pooveshn6566@gmail.com>
2025-09-10 14:49:31 +0530

    Add files via upload

---
+++ q6.cpp
@@ -0,0 +1,10 @@
+# include <stdio.h>
+int main (){
+	int a,b;
+	scanf("%d%d",&a,&b);
+	int c= a;
+	a =b;
+	b=c;
+	printf("%d%d", a,b);
+	return 0 ;
+}
---
+++ q8.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(){
+	int a=53 ,b=23;
+//	scanf("%d",&a);
+//	printf("%d\t",a);
+	printf("%d",a>>);
+	return 0 ;
+}
